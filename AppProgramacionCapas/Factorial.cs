5676218a97742e79a88e6e66ab08750600ac9ad4
alejorb <luis.rodriguezbenalcazar@pucese.edu.ec>
2019-11-14 09:40:25 -0500

    Agregar archivos de proyecto.

---
+++ AppProgramacionCapas/Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProgramacionCapas
+{
+    class Factorial
+    {
+        public static long fact (int num)
+        {
+            long f = 1;
+            if (num == 0 || num == 1)
+                return f;
+
+            for (int = 1; int <= num; int++)
+                f = f * 1;
+
+            return f;
+        }
+    }
+}
